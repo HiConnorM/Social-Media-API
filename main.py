db62646fb79f54a29a8568f44ac9c3c1d15009dc
Connor Morgan <theconnorm@gmail.com>
2022-05-18 12:09:21 -0500

    setup for api

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str 
+    content: str 
+    published: bool = True 
+    rating: Optional[int] = None
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    
+    return {"Name": "Connor"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is a post"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    return {"data": "new post"}
+
+# title str, content str, category, bool published or draft
+
+
